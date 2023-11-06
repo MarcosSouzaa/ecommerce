41fd89838e826071264ebd411e0608d64eb998c1
MarcosSouzaa <marcossouza.git@gmail.com>
2023-11-05 22:04:15 -0300

    Criada a página produtos e inserida a estilização da home

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def page_home():
     return render_template('home.html')
 
- 
+@app.route('/produtos')
+def page_produto():
+    return render_template('produtos.html')
