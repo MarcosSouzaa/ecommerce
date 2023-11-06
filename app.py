dc9f9f3104acc86ab1107f1f4932c446b03eb575
MarcosSouzaa <marcossouza.git@gmail.com>
2023-11-06 20:03:21 -0300

    Dicionando produtos em lista de dicionários estáticos e recuperando na página produtos

--- app.py
+++ app.py
@@ -8,4 +8,10 @@ def page_home():
 
 @app.route('/produtos')
 def page_produto():
-    return render_template('produtos.html')
+    itens = [
+       {'id': 1, 'nome':'Celular', 'codi_barra':'234568789654','preco': 1200},
+       {'id': 2, 'nome':'Notebook', 'cod_barra':'121245445454','preco': 3500},
+       {'id': 3, 'nome':'Teclado', 'cod_barra':'325645678965','preco': 120},
+       {'id': 4, 'nome': 'Monitor', 'cod_barra':'565654879991','preco':800}
+    ]     
+    return render_template('produtos.html', itens = itens)
