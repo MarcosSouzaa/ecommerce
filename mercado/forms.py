cc9465afae6b5daa24affdf8f1d36e87e7d39263
MarcosSouzaa <marcossouza.git@gmail.com>
2023-11-21 22:18:17 -0300

    inserido validações e bibliotecas necessárias

--- mercado/forms.py
+++ mercado/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 # Vai extender do FlaskForm e será um formulário
 class CadastroForm(FlaskForm):
-    usuario = StringField(label='Username:')
-    email = StringField(label='E-mail:')
-    senha1 = PasswordField(label='Senha:')
-    senha2 = PasswordField(label='Confirmação de senha:')
+    usuario = StringField(label='Username:', validators=[Length(min=2, max=30), DataRequired()])
+    email = StringField(label='E-mail:', validators=[Email(), DataRequired()])
+    senha1 = PasswordField(label='Senha:', validators=[Length(min=6 ), DataRequired()])
+    senha2 = PasswordField(label='Confirmação de senha:', validators=[EqualTo('senha1'), DataRequired()])
     submit = SubmitField(label='Cadastrar')
     
